eb451d0dcdbf12f384ba8c4e47fd6189414c979f
Carina Jin Hertel <carina-jin.hertel@stud.hs-hannover.de>
2025-04-10 17:13:51 +0200

    setup leaflet

---
+++ src/SimpleMap.js
@@ -0,0 +1,40 @@
+import React, { useRef } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    iconSize: [25,41],
+    iconAnchor: [12,41]
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+const SimpleMap = () => {
+    const mapRef = useRef(null);
+    const latitude = 52.7269;
+    const longitude = 8.2857;
+    const position = [latitude, longitude];
+  
+    return ( 
+        <MapContainer center={[latitude, longitude]} zoom={13} ref={mapRef} style={{height: "100vh", width: "100vw"}}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+          <Popup offset={[0, -30]}>
+            A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+
+          </Marker>
+        </MapContainer>
+    );
+};
+
+export default SimpleMap;
