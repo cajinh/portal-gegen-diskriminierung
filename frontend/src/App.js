7cbe1a631851583a77144cd60db3a4f1ebf23253
Carina Jin Hertel <carina-jin.hertel@stud.hs-hannover.de>
2025-04-14 09:57:50 +0200

    setup prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
-import React, { useEffect, useState }  from 'react';
+import React, { useEffect, useState } from 'react';
 import SimpleMap from './SimpleMap';
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState('');
   useEffect(() => {
-    fetch("http://localhost:8080/hello")
-        .then(response => response.text())
-        .then(data => setMessage(data));
-}, []);
+    fetch('http://localhost:8080/hello')
+      .then((response) => response.text())
+      .then((data) => setMessage(data));
+  }, []);
 
   return (
     <div>
       <h1>My Leaflet.js and React Map</h1>
       <div className="App">
-            <h1>{message}</h1>
+        <h1>{message}</h1>
       </div>
       <SimpleMap />
     </div>
