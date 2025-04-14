46d37aeb1353c7ccc89e3e7d503a3122d0b7215f
Carina Jin Hertel <carina-jin.hertel@stud.hs-hannover.de>
2025-04-14 09:30:08 +0200

    added backend

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Meldeportal gegen Diskriminierung - Bachelorprojekt 
+
+## Backend
+
+To start the backend:
+./mvnw quarkus:dev
+
+
+## Frontend
+
+Start dev server:
+npm start 
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState }  from 'react';
 import SimpleMap from './SimpleMap';
 
 function App() {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    fetch("http://localhost:8080/hello")
+        .then(response => response.text())
+        .then(data => setMessage(data));
+}, []);
+
   return (
     <div>
       <h1>My Leaflet.js and React Map</h1>
+      <div className="App">
+            <h1>{message}</h1>
+      </div>
       <SimpleMap />
     </div>
   );
